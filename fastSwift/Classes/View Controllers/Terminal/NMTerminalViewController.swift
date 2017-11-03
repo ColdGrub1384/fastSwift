a7afca471c0a162706eb2bd66d2ad6e95ce7d441
Adrian Labbe <adrilabbelol@gmail.com>
2017-11-03 19:27:39 -0300

    Re-added consoleHTML due to attributed string bug ;(, but CSS bug still fixed

--- fastSwift/Classes/View Controllers/Terminal/NMTerminalViewController.swift
+++ fastSwift/Classes/View Controllers/Terminal/NMTerminalViewController.swift
@@ -35,6 +35,8 @@ class NMTerminalViewController: UIViewController, NMSSHSessionDelegate, NMSSHCha
         
     }
     
+    var consoleHTML = ""
+    
     func navigationControllerSupportedInterfaceOrientations(_ navigationController: UINavigationController) -> UIInterfaceOrientationMask {
         return .portrait
     }
@@ -144,8 +146,9 @@ class NMTerminalViewController: UIViewController, NMSSHSessionDelegate, NMSSHCha
         DispatchQueue.main.async {
             
             self.terminal.text = (self.terminal.text+message).replacingOccurrences(of: "\n", with: "<br/>")
+            self.consoleHTML = self.consoleHTML+message.replacingOccurrences(of: "\n", with: "<br/>")
             
-            if let html = (self.terminal.text+self.terminalHTML).attributedStringFromHTML {
+            if let html = (self.consoleHTML+self.terminalHTML).attributedStringFromHTML {
                 self.terminal.attributedText = html
             }
             
@@ -153,9 +156,16 @@ class NMTerminalViewController: UIViewController, NMSSHSessionDelegate, NMSSHCha
             
             if self.terminal.text.contains("Program output") { // Clear shell
                 let newString = self.terminal.text.components(separatedBy: "Program output")
+                let newHTML = self.consoleHTML.components(separatedBy: "Program output")
+                
                 self.activity.stopAnimating()
                 self.terminal.text = newString[1]
+                self.consoleHTML = newHTML[1]
                 self.console = self.terminal.text
+                
+                if let html = (self.consoleHTML+self.terminalHTML).attributedStringFromHTML {
+                    self.terminal.attributedText = html
+                }                
             }
             
             if self.terminal.text.contains("<showAlert>") { // Show an alert
@@ -194,6 +204,10 @@ class NMTerminalViewController: UIViewController, NMSSHSessionDelegate, NMSSHCha
                 self.terminal.text = self.terminal.text.replacingFirstOccurrence(of: self.terminal.text.slice(from: "<showAlert>", to: "</showAlert>")!, with: "")
                 self.terminal.text = self.terminal.text.replacingFirstOccurrence(of: "<showAlert>", with: "")
                 self.terminal.text = self.terminal.text.replacingFirstOccurrence(of: "</showAlert>", with: "")
+                
+                self.consoleHTML = self.consoleHTML.replacingFirstOccurrence(of: self.consoleHTML.slice(from: "&lt;showAlert&gt;", to: "&lt;/showAlert&gt;")!, with: "")
+                self.consoleHTML = self.consoleHTML.replacingFirstOccurrence(of: "&lt;showAlert&gt;", with: "")
+                self.consoleHTML = self.consoleHTML.replacingFirstOccurrence(of: "&lt;/showAlert&gt;", with: "")
             }
             
             if self.terminal.text.contains("<theme>") {
@@ -260,13 +274,14 @@ class NMTerminalViewController: UIViewController, NMSSHSessionDelegate, NMSSHCha
                 self.activity.startAnimating()
                 self.terminal.text = self.terminal.text.replacingOccurrences(of: "Show activity", with: "")
                 self.console = self.terminal.text
+                self.consoleHTML = self.consoleHTML.replacingOccurrences(of: "Show activity", with: "")
             }
             
             if self.terminal.text.contains("Hide activity") {
                 self.activity.stopAnimating()
                 self.terminal.text = self.terminal.text.replacingOccurrences(of: "Hide activity", with: "")
                 self.console = self.terminal.text
-                
+                self.consoleHTML = self.consoleHTML.replacingOccurrences(of: "Show activity", with: "")
             }
             
             if self.terminal.text.contains("DownloadBinaryFileNow") {
